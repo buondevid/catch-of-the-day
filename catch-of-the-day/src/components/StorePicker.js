844c0dab67063868a89ee3fb6a1a8825c193a0e7
buondevid <davidebriano@gmail.com>
2021-06-16 19:21:58 +0200

    add entry screen StorePicker

--- catch-of-the-day/src/components/StorePicker.js
+++ catch-of-the-day/src/components/StorePicker.js
@@ -2,14 +2,26 @@ import React from 'react';
 import { getFunName } from '../helpers';
 
 class StorePicker extends React.Component {
-	render () {
+
+	myInput = React.createRef();
+
+	goToStore = (e) => {
+		//1. stop form from submitting
+		e.preventDefault();
+		//2. get text from that input
+		const storeName = this.myInput.current.value;
+		//3. change page to /store/whatever-they-entered
+		this.props.history.push(`/store/${storeName}`);
+	}
+
+	render() {
 		return (
-			<form className='store-selector'>
+			<form onSubmit={this.goToStore} className='store-selector'>
 				<h2>Please Enter a Store</h2>
-				<input type='text' required placeholder='Store Name' defaultValue={getFunName()}/>
+				<input ref={this.myInput} type='text' required placeholder='Store Name' defaultValue={getFunName()} />
 				<button type='submit'>Visit Store</button>
 			</form>
-		)	 
+		);
 	}
 }
 
